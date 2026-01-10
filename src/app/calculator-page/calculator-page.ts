ba848ead2760456355adda0804a1945be58d572d
Saristeidyn <saray437933@gmail.com>
2026-01-09 21:22:42 -0600

    did the buttons. the toggle and buttons still needs styling tho

--- src/app/calculator-page/calculator-page.ts
+++ src/app/calculator-page/calculator-page.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Needed for *ngIf, *ngFor, etc.
-
 
 @Component({
   selector: 'calculator-page',
-  standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './calculator-page.html',
-  styleUrls: ['./calculator-page.scss'],
+  styleUrl: './calculator-page.scss',
 })
 export class CalculatorPage {
 
